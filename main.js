1e634067a2ba01679e0c35e1bad3ef36bf0fe97e
juanpabloo198921 <juanpablo198921@gmail.com>
2018-06-25 17:54:50 -0700

    javascript

---
+++ main.js
@@ -0,0 +1,37 @@
+var fecha_solicitud="20-06-2018";
+
+var nombre="pepito perez";
+
+var fecha_nacimiento="20-04-2001";
+
+var email="nombre@gmail.com";
+
+var telefono="2221425";
+
+var genero="masculino";
+
+var departamento="antioquia";
+
+var ciudad="medellin";
+
+var direccion="Cra 22 #22c -22";
+
+var tipo_de_domicilio="casa";
+
+var integrantes_del_domicilio=3;
+
+var Por_qué_quieres_adoptar_una_mascota="porque quiero compañia";
+
+var tienes_experiencia_con_animales="si";
+
+var tienes_otras_mascotas="si";
+
+var ingresos_mensuales=500000;
+
+var cuantos_ingresos_dispones_para_tu_mascota=50000;
+
+var cuanto_tiempo_piensas_dedicarle_a_tu_mascota="3 horas al dia";
+
+var quién_cuidará_de_tu_mascota="mi abuelita";
+		
+
